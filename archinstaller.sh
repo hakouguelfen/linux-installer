29d224f08c5f07273c8fc2c04036576517471adb
hakou guelfen <hakoudev@gmail.com>
2022-08-31 12:52:46 +0100

    change yay to paru

--- archinstaller.sh
+++ archinstaller.sh
@@ -2,11 +2,11 @@
 
 sudo pacman -S base-devel xorg picom nitrogen zsh qtile alacritty lsd bat neovim vivid nautilus mypy python-pip python-iwlib rofi vlc transmission-gtk zsh-syntax-highlighting zsh-autosuggestions alsa-utils light zip unzip ripgrep fd emacs most
 
-## install yay
-git clone https://aur.archlinux.org/yay-git.git yay && cd yay || exit
+## install paru
+git clone https://aur.archlinux.org/paru.git paru && cd paru || exit
 makepkg -si
 cd ..
-rm -rf yay
+rm -rf paru
 
 # install doom emacs
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
@@ -17,14 +17,14 @@ yay -S nerd-fonts-mononoki ttf-meslo-nerd-font-powerlevel11k nvim-packer-git col
 sudo pacman -S ttf-fira-code ttf-font-awesome
 
 # install login manager
-yay -S ly 
+paru -S ly 
 sudo systemctl enable ly
 
 #install some apps
-yay -S brave-bin spotify flameshot nomacs htop neofetch lxappearance qt5ct nm-connection-editor 
+paru -S brave-nightly-bin spotify flameshot nomacs htop neofetch lxappearance qt5ct nm-connection-editor 
 
 # configure zsh
-yay -S --noconfirm zsh-theme-powerlevel10k-git
+paru -S --noconfirm zsh-theme-powerlevel10k-git
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
 chsh -s /bin/zsh root
