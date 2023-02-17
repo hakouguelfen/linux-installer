847ab9a11e10a5cd0554442774b4f9ba5692076f
hakou guelfen <hakoudev@gmail.com>
2023-02-17 02:24:22 +0100

    feat: add dunst

--- archinstaller.sh
+++ archinstaller.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -S base-devel xorg picom nitrogen zsh qtile alacritty lsd bat zoxide fzf neovim vivid  mypy python-pip python-iwlib rofi vlc transmission-gtk zsh-syntax-highlighting zsh-autosuggestions alsa-utils light zip unzip ripgrep fd emacs most zathura zathura-pdf-poppler conky abiword github-cli
-
+sudo pacman -S base-devel xorg picom nitrogen zsh qtile alacritty lsd bat zoxide fzf neovim vivid  mypy python-pip python-iwlib rofi vlc transmission-gtk zsh-syntax-highlighting zsh-autosuggestions alsa-utils light zip unzip ripgrep fd emacs most zathura zathura-pdf-poppler conky abiword github-cli dunst
 sudo pacman -S --needed hsetroot
 
 ## install paru
