b402d0abd535f16daff9eefcfa65fa8431284b0b
hakouguelfen <hakoudev@gmail.com>
2024-03-24 02:59:27 +0100

    remove rofi

--- archinstaller.sh
+++ archinstaller.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -S zsh alacritty lsd bat zoxide fzf neovim rofi zsh-syntax-highlighting zsh-autosuggestions zip unzip ripgrep fd emacs-nativecmop most zathura zathura-pdf-poppler dunst mpv pcmanfm fuzzel hyprpaper hyprland waybar brightnessctl ly qbittorrent htop grim slurp stow imv mat2 onionshare nwg-look thunderbird
+sudo pacman -S zsh alacritty lsd bat zoxide fzf neovim zsh-syntax-highlighting zsh-autosuggestions zip unzip ripgrep fd emacs-nativecmop most zathura zathura-pdf-poppler dunst mpv pcmanfm fuzzel hyprpaper hyprland waybar brightnessctl ly qbittorrent htop grim slurp stow imv mat2 onionshare nwg-look thunderbird calibre
 sudo pacman -S xdg-desktop-portal-hyprland github-cli discord libsecret udiskie polkit-kde-agent qt5-wayland qt6-wayland
 sudo pacman -S --needed hsetroot
 
@@ -25,13 +25,13 @@ paru -S colloid-gtk-theme-git colloid-icon-theme-git
 sudo pacman -S ttf-fira-codettf-firacode-nerd 
 
 #install some apps
-paru -S librewolf-bin nuclear-player-bin 
+paru -S librewolf-bin nuclear-player-bin spotify-bin
 
 ########################################
 ########### clone my dotfiles ##########
 ########################################
-git clone https://github.com/hakouklvn/wayland_dotfiles
-stow wayland_dotfiles/*  
+git clone https://github.com/hakouklvn/wayland_dotfiles dotfiles
+stow dotfiles/*
 git clone https://github.com/hakouklvn/scripts .local/bin/
 git clone https://github.com/hakouklvn/wallpaper /usr/share/
 
