6863bd4c55fcb14a8d2e2d4a77134260fe296842
hakouguelfen <hakoudev@gmail.com>
2024-06-12 23:48:39 +0100

    update to script

--- archinstaller.sh
+++ archinstaller.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sudo pacman -S zsh alacritty lsd bat zoxide fzf neovim zsh-syntax-highlighting zsh-autosuggestions zip unzip ripgrep fd emacs-nativecomp most zathura zathura-pdf-poppler dunst mpv pcmanfm-gtk3 brightnessctl ly qbittorrent htop stow imv vivid flameshot wget
+sudo apt install zsh alacritty lsd bat zoxide fzf neovim zsh-syntax-highlighting zsh-autosuggestions zip unzip ripgrep emacs most zathura zathura-pdf-poppler dunst mpv pcmanfm brightnessctl htop stow imv vivid flameshot wget tmux pipewire
 #sudo pacman -S xdg-desktop-portal-hyprland github-cli discord libsecret udiskie polkit-kde-agent qt5-wayland qt6-wayland fuzzel hyprpaper hyprland waybar grim slurp nwg-look 
-sudo pacman -S --needed hsetroot
+#sudo pacman -S --needed hsetroot
 
 ########################################
 ########### Install paru ###############
@@ -19,11 +19,15 @@ git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom install
 
 ########################################
+########### Install tmux ###############
+########################################
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+########################################
 ######### install fonts  ###############
 ########################################
-mkdir .fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip .fonts/
-cd .fonts
+mkdir .fonts && cd .fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip 
 unzip FiraCode.zip
 rm -rf FiraCode.zip
 cd
@@ -33,8 +37,8 @@ cd
 ########################################
 git clone https://github.com/vinceliuice/Colloid-gtk-theme
 git clone https://github.com/vinceliuice/Colloid-icon-theme
-sh Colloid-gtk-theme/install.sh -t grey -c dark -l --tweaks nord 
-sh Colloid-icon-theme/install.sh -s nord -t grey
+bash Colloid-gtk-theme/install.sh -t grey -c dark -l --tweaks nord 
+bash Colloid-icon-theme/install.sh -s nord -t grey
 
 rm -rf Colloid-gtk-theme
 rm -rf Colloid-icon-theme
@@ -43,9 +47,11 @@ rm -rf Colloid-icon-theme
 ########### clone my dotfiles ##########
 ########################################
 git clone https://github.com/hakouklvn/wayland_dotfiles dotfiles
+cd dotfiles
 stow -t ~ --ignore=README.md .
-git clone https://github.com/hakouklvn/scripts .local/bin/scripts
-sudo git clone https://github.com/hakouklvn/wallpaper /usr/share/wallpaper
+cd
+#git clone https://github.com/hakouklvn/scripts .local/bin/scripts
+#sudo git clone https://github.com/hakouklvn/wallpaper /usr/share/wallpaper
 
 ########################################
 ########### change bash shell ##########
@@ -53,3 +59,20 @@ sudo git clone https://github.com/hakouklvn/wallpaper /usr/share/wallpaper
 chsh -s /bin/zsh root
 chsh -s /bin/zsh "$USER"
 
+# change keyboard layout to azerty
+echo '
+Section "InputClass"
+       Identifier "system-keyboard"
+       MatchIsKeyboard "on"
+       Option "XkbLayout" "fr"
+       Option "XkbModel" "pc105"
+       Option "XkbOptions" "terminate:ctrl_alt_bksp"
+EndSection' > sudo /etc/X11/xorg.conf.d/00-keyboard.conf
+
+## enable touchpad 
+echo '
+Section "InputClass"
+   Identifier "libinput touchpad catchall"
+   Driver "libinput"
+   Option "Tapping" "on"
+EndSection' > sudo /etc/X11/xorg.conf.d/40-libinput.conf
